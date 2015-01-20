0135ecaac4dd8340c1b5bcaa0f7f4e0b5c74229b
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2015-01-20 16:21:38 +0100

    Added package documentation

---
+++ R/HierarchicalEdgeBundles-package.R
@@ -0,0 +1,19 @@
+#' Visualize graphs using Hierarchical Edge Bundles
+#'
+#' Visualize graphs using a crude version of the hierarchical edge bundling 
+#' method described in the reference below.
+#'
+#' @name HierarchicalEdgeBundles-package
+#' @aliases HierarchicalEdgeBundles-package HierarchicalEdgeBundles HEB
+#' @author
+#'   Anders Ellern Bilgrau \cr
+#'   Maintainer: Anders Ellern Bilgrau <abilgrau@@math.aau.dk>
+#' @docType package
+#' @references
+#'   Holten, Danny. "Hierarchical edge bundles: Visualization of adjacency 
+#'   relations in hierarchical data." Visualization and Computer Graphics, IEEE 
+#'   Transactions on 12.5 (2006): 741-748.
+#' @seealso
+#'   Core user function: \code{\link{plotHierarchicalEdgeBundles}}
+#' @import igraph
+NULL
